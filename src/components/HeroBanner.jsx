b37381a8944e4e9008ced703110357ebe852ea4a
Emmanuel Balogun <emmanx25@gmail.com>
2024-03-04 21:04:17 +0100

    new commit

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React from "react";
+import Heropic from "../assets/Hero.jpeg";
 
 const HeroBanner = () => {
   return (
-    <div className=' relative p-[30px] md:mt-20' >
-        <p className='text-3xl font-Marker font-[500]'>Transform your body</p>
-        <p>Sweat, smile and repeat</p>
-        <p>Explore the most effective exercises</p>
+    <div className="flex relative p-[30px] mt-10 md:mt-16">
+      <div className="w-full">
+        <p className="text-2xl md:text-3xl font-Marker font-[500] text-[#001219]">
+          Transform your body
+        </p>
+        <p className="font-[900] text-6xl md:text-8xl font-Ojuju text-[#001219]">
+          Sweat, smile <br /> and repeat
+        </p>
+        <p className="font-Marker pt-4 text-xl text-[#001219]">
+          Explore the most effective exercises
+        </p>
+        <button
+          className="bg-[#ffd60a] shadow-md h-[43px] w-[140px] font-[600] font-Marker mt-6  rounded-[5px]"
+          href="#exercises"
+        >
+          Click here
+        </button>
+
+        
+          <p className="font-[700] hidden lg:block text-center text-[200px] font-Ojuju opacity-[0.2] text-[#ffd60a] mt-[-60px]">
+            Exercise
+          </p>
+
+      </div>
+
+      <img
+        src={Heropic}
+        alt="banner"
+        className="absolute hidden lg:block right-[0px] top-[0px] mt-[-140px] h-screen w-auto"
+      />
     </div>
-  )
-}
+  );
+};
 
 export default HeroBanner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div className=' flex justify-around md:justify-normal md:gap-40'>
+    <div className=' flex justify-around md:justify-normal md:gap-40 mt-5 md:mt-0'>
       <Link to={"/"}>
-        <h2 className='text-3xl font-[800] font-Ojuju m-[20px] text-[#001219]'>Stay-Fit</h2>
+        <h2 className='text-3xl font-[800] font-Ojuju md:m-[20px] text-[#001219]'>Stay-Fit</h2>
       </Link>
 
       <div className=' flex gap-5 items-center'>
-        <Link to={"/"} className='font-Ojuju font-[750] text-xl'> Home </Link>
+        <Link to={"/"} className='font-Ojuju font-[750] text-xl border-b-4 border-[#ffd60a]'> Home </Link>
         <a href="#exercises" className='font-Ojuju font-[750] text-xl text-[#001219]'>Exercises</a>
       </div>
     </div>
